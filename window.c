1385a96d05edf6739bbda674250f369d57b68953
wizard-lgtm <rippiten23@gmail.com>
2024-09-28 17:47:18 +0300

    entries added hover added

--- window.c
+++ window.c
@@ -1,4 +1,5 @@
 #include<SDL2/SDL.h>
+#include "fops.h"
 
 #define WINDOW_W 400
 #define WINDOW_H 600
@@ -7,30 +8,52 @@
 #define BG_G 0xFF
 #define BG_B 0xFF
 
+SDL_Surface* surface;
+SDL_Window* window;
+
 typedef enum {
     COLOR_PASTEL_BLUE,
     COLOR_GRAY,
     COLOR_WHITE,
-    COLOR_TOTAL // track number
-
+    COLOR_BLACK,
+    COLOR_RED,
+    COLOR_GREEN,
+    COLOR_YELLOW,
+    COLOR_ORANGE,
+    COLOR_PURPLE,
+    COLOR_TOTAL // track number of colors
 } ColorPaletteDark;
 
 Uint32 colors_dark[COLOR_TOTAL];
 
 void initialize_colors(SDL_Surface* surface) {
-    // Her rengi SDL_MapRGB ile tanımlıyoruz
+    // Initialize colors using SDL_MapRGB
     colors_dark[COLOR_PASTEL_BLUE] = SDL_MapRGB(surface->format, 0xA7, 0xC7, 0xE7);
-    colors_dark[COLOR_GRAY] = SDL_MapRGB(surface->format, 0x72, 0x72, 0xF7);
+    colors_dark[COLOR_GRAY] = SDL_MapRGB(surface->format, 0x72, 0x72, 0x72);   // Fixed the gray color value
     colors_dark[COLOR_WHITE] = SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF);
+    colors_dark[COLOR_BLACK] = SDL_MapRGB(surface->format, 0x00, 0x00, 0x00);
+    colors_dark[COLOR_RED] = SDL_MapRGB(surface->format, 0xFF, 0x00, 0x00);
+    colors_dark[COLOR_GREEN] = SDL_MapRGB(surface->format, 0x00, 0xFF, 0x00);
+    colors_dark[COLOR_YELLOW] = SDL_MapRGB(surface->format, 0xFF, 0xFF, 0x00);
+    colors_dark[COLOR_ORANGE] = SDL_MapRGB(surface->format, 0xFF, 0xA5, 0x00);
+    colors_dark[COLOR_PURPLE] = SDL_MapRGB(surface->format, 0x80, 0x00, 0x80);
 }
 
 /// Draws entry
 ///
-void draw_list_entry(){
+void draw_list_entry(int x, int y){
+    int w = WINDOW_W - 50;
+    int h = 50;
+    SDL_Rect rect;
+    rect.w = w;
+    rect.h = h;
+    rect.x = x;
+    rect.y = y;
 
+    SDL_FillRect(surface, &rect, colors_dark[COLOR_WHITE]);
 }
 
-void draw_trackbar(SDL_Surface* surface){
+SDL_Rect draw_trackbar(){
     int w = 10;
     int h = WINDOW_H / 4; // always %20
     int x = WINDOW_W - PADDING - w;
@@ -39,13 +62,37 @@ void draw_trackbar(SDL_Surface* surface){
     SDL_Rect trackbar = {x, y, w, h}; 
 
     SDL_FillRect(surface, &trackbar, colors_dark[COLOR_PASTEL_BLUE]);
+    return trackbar;
+}
+void update_window(){
+    SDL_UpdateWindowSurface(window);
+}
+void trackbar_hover(SDL_Rect* rect){
+    SDL_FillRect(surface, rect, colors_dark[COLOR_GRAY]);
+}
 
+void handle_scroll(){
+    
+}
+
+typedef void (*HoverHandler)(SDL_Rect*);
+void handle_hover_rect(SDL_Rect* rect, HoverHandler handler){
+    int mouse_x, mouse_y;
+    SDL_GetMouseState(&mouse_x, &mouse_y);
+
+
+    if(mouse_x > rect->x && mouse_x < rect->x + rect->w
+    && mouse_y > rect->y && mouse_y < rect->y + rect->h
+    ){
+        printf("Hover!");
+        handler(rect);
+    }
 }
 
 /// 
 /// Draws list for dir entries
 ///
-void draw_list(SDL_Surface* surface){
+void draw_list(char** list){
     // declare widget 
     int border_size = 10;
     SDL_Rect list_widget;
@@ -55,26 +102,46 @@ void draw_list(SDL_Surface* surface){
     list_widget.x = PADDING;
     list_widget.y = PADDING;
     
-    Uint32 color = SDL_MapRGB(surface->format, 0x72, 0x72, 0x72); // pastel gray
+    
 
-    SDL_FillRect(surface, &list_widget, color);
+    SDL_FillRect(surface, &list_widget, colors_dark[COLOR_ORANGE]);
+
+
+    for (int i = 0; list[i] != NULL; i++) {
+        int y;
+        int x;
+        if(i == 0) y = PADDING + 5;
+        else y = i * 75 + 5;
+        x = PADDING + 20;
+
+        printf("Entry %d: %s\n", i + 1, list[i]);
+        draw_list_entry(x, y);
+    }
+
 
     // declare border
 }
 
-void draw_background(SDL_Surface* surface){
+void draw_background(){
     SDL_Rect bg;
     bg.h = WINDOW_H;
     bg.w = WINDOW_W;
     bg.x = 0;
     bg.y = 0;
-    Uint32 white = SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF);
+    
 
-    SDL_FillRect(surface, &bg, white);
+    SDL_FillRect(surface, &bg, colors_dark[COLOR_WHITE]);
+}
+
+char* change_dir(char* current_dir, char* new_dir){
+    char* new_ptr = realloc(current_dir, strlen(new_dir)+ 1);
+    return new_ptr;
 }
 
 void root(){
-
+    char* current_dir = malloc(strlen(".") + 1 );
+    strcpy(current_dir, ".");
+    char** list = list_dir_entries(current_dir);
 
     // Init sdl
     SDL_Init(SDL_INIT_VIDEO);
@@ -90,14 +157,15 @@ void root(){
 
     initialize_colors(surface);
 
-    draw_background(surface);
-    draw_list(surface);
-    draw_trackbar(surface);
+    draw_background();
+    draw_list(list);
+    SDL_Rect trackbar = draw_trackbar();
 
     SDL_UpdateWindowSurface(window);
 
     SDL_Event event;
     while(running){
+        handle_hover_rect(&trackbar, trackbar_hover); 
         while(SDL_PollEvent(&event))
         if(event.type == SDL_QUIT){
             running = 0;
@@ -106,5 +174,11 @@ void root(){
     }
     // Exit 
     SDL_DestroyWindow(window);
+    free(current_dir);
+    // Free list
+    for (int i = 0; list[i] != NULL; i++) {
+        free(list[i]); // Correctly free each entry
+    }
+    free(list); // Finally, free the list itself
     SDL_Quit();
 }
